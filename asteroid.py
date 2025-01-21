03756dd2feae85b46697c705e9176aabab30c889
Peridan9 <Danielperi9@gmail.com>
2025-01-21 23:33:39 +0200

    ADDED shots and made asteroids to split

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 from circleshape import CircleShape
 import pygame
+import random
+from constants import *
 
 class Asteroid(CircleShape):
     def __init__(self,x,y,radius):
@@ -10,3 +12,18 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        ran = random.uniform(20,50)
+        vec1 = self.velocity.rotate(ran)
+        vec2 = self.velocity.rotate(-ran)
+        nR = self.radius - ASTEROID_MIN_RADIUS
+        as1 = Asteroid(self.position.x,self.position.y,nR)
+        as2 = Asteroid(self.position.x,self.position.y,nR)
+        as1.velocity = vec1 * 1.2
+        as2.velocity = vec2 * 1.2
+
+        
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
 
@@ -18,10 +19,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable,drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots,updatable, drawable)
 
     asteroidfield = AsteroidField()
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
@@ -36,8 +39,15 @@ def main():
             object.update(dt)
 
         for astreo in asteroids:
+            for shot in shots:
+                if astreo.collision(shot):
+                    astreo.split() 
+                    shot.kill()
+
+        for astreo in asteroids:
             if astreo.collision(player):
-                sys.exit("Game over!")  
+                sys.exit("Game over!")
+ 
 
         screen.fill("black")
 
