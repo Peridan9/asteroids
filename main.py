9d6d8ed0e7f999fd269202c83fc138f8d3918e09
Peridan9 <Danielperi9@gmail.com>
2025-01-21 17:23:05 +0200

    UPDATED: main and player added rotation

--- main.py
+++ main.py
@@ -18,13 +18,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
+            
+        player.update(dt)
 
+        screen.fill("black")
         player.draw(screen)
+        pygame.display.flip()
+
         dt = clock.tick(60) / 1000  # Convert milliseconds to seconds
 
-        
-        pygame.display.flip()
 
         
 
