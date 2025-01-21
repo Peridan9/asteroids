b45fcc1a83198526e2ce40da9459910b423ecaf7
Peridan9 <Danielperi9@gmail.com>
2025-01-21 17:38:13 +0200

    UPDATED: added foward and back option and rapped update and draw in containers

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_WIDTH))
     clock = pygame.time.Clock()
     dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable,drawable)
+
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
 
     # game loop
@@ -18,11 +24,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        player.update(dt)
+
+        for object in updatable:
+            object.update(dt)     
 
         screen.fill("black")
-        player.draw(screen)
+
+        for object in drawable:
+            object.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000  # Convert milliseconds to seconds
